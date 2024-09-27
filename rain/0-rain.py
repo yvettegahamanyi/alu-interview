13808e069ccfa4286395608426fa4c1b2b8421d2
yvettegahamanyi <ygahamanyi26@gmail.com>
2024-09-27 18:54:35 +0200

    fix: PEP8 validation v3

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -11,6 +11,7 @@ Function:
 """
 
 def rain(walls):
+    
     """
     Parameters:
         - walls (List[int]): a list of non-negative integers
@@ -35,7 +36,7 @@ def rain(walls):
         left_max[i] = max(left_max[i - 1], walls[i])
 
     right_max[n - 1] = walls[n - 1]
-    for i in range(n-2, -1,-1):
+    for i in range(n-2, -1, -1):
         right_max[i] = max(right_max[i + 1], walls[i])
 
     water_retained = 0
