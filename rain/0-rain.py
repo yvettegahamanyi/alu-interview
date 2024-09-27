acba4b4a0131f718a285159f9dfd39905cc3f7f6
yvettegahamanyi <ygahamanyi26@gmail.com>
2024-09-27 18:46:13 +0200

    ft: rain

---
+++ rain/0-rain.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" 
+Given a list of non-negative integers representing the heights of walls with 
+unit width 1, as if viewing the cross-section of a relief map, calculate how 
+many square units of water will be retained after it rains.
+
+Function: 
+    - rain(walls): returns the total amount of rainwater retained given a list
+    of walls
+
+"""
+
+def rain(walls):
+    """
+    Parameters:
+        - walls (List[int]): a list of non-negative integers 
+        to represent the height of the wall
+    Example: 
+        walls = [0, 1, 0, 2, 0, 3, 0, 4]
+        print(rain(walls)) = 6
+
+        walls = [2, 0, 0, 4, 0, 0, 1, 0]
+        print(rain(walls))= 6
+    
+    """
+    if not walls:
+        return 0
+    
+    n = len(walls)
+    left_max = [0] * n
+    right_max = [0] * n
+    
+    left_max[0] = walls[0]
+    for i in range(1, n):
+        left_max[i] = max(left_max[i - 1], walls[i])
+        
+    right_max[n - 1] = walls[n - 1]
+    for i in range(n-2, -1,-1):
+        right_max[i] = max(right_max[i + 1], walls[i])
+        
+    water_retained = 0
+    for i in range(n):
+        water_retained += min(left_max[i], right_max[i]) - walls[i]
+    
+    return water_retained
---
+++ rain/README.md
@@ -0,0 +1,11 @@
+# Rain Question
+
+Given a list of non-negative integers representing the heights of walls with unit width 1, as if viewing the cross-section of a relief map, calculate how many square units of water will be retained after it rains.
+
+Visual representation of the walls `[0, 1, 0, 2, 0, 3, 0, 4]`
+
+![alt text](https://s3.amazonaws.com/alu-intranet.hbtn.io/uploads/medias/2021/4/85ef782020ac6efdc7004b62ea86724a552285b4.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIARDDGGGOUZTW2RLVB%2F20240927%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240927T144551Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=cf4b5a9c9bec3083355bbccc6014d07df98dbd89eac1b2a81271b25bab8196f1)
+
+Visual representation of the walls `[2, 0, 0, 4, 0, 0, 1, 0]`
+
+![alt text](https://s3.amazonaws.com/alu-intranet.hbtn.io/uploads/medias/2021/4/9a27c3e4e214e55b3c0b8b1439fdc99b4a184ff5.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIARDDGGGOUZTW2RLVB%2F20240927%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240927T144551Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=8fd0a028e1ec64c89fb3c637ec0c43a818606df65bb6d1ed3acddee534b03633)
